3ccb8212a9214dc473afd52975605b3e78001f60
anza04 <64538739+anza04@users.noreply.github.com>
2023-07-18 10:55:23 +0200

    cambio classifica

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -17,7 +17,7 @@ export default function Homepage(){
             <div className="foto" id='anza'></div><div className="nome">2. The Anza</div>
         </div>
         <div className="riga">
-            <div className="foto" id='broggi'></div><div className="nome">3. 8roggi</div>
+            <div className="foto" id='faso'></div><div className="nome">3. Faso</div>
         </div>
         <div className="riga">
             <div className="foto" id='garna'></div><div className="nome">4. Garna the Gabri</div>
@@ -26,7 +26,7 @@ export default function Homepage(){
             <div className="foto" id='trenta'></div><div className="nome">5. Trenta i</div>
         </div>
         <div className="riga">
-            <div className="foto" id='faso'></div><div className="nome">6. Faso</div>
+            <div className="foto" id='broggi'></div><div className="nome">6. 8roggi</div>
         </div>
         <div className="riga">
             <div className="foto" id='onorevole'></div><div className="nome">7. Onorevole</div>
