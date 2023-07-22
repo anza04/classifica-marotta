3ef9468ca1684ad98f783f3af3dd7b64114253da
Federico Anzani <federico.anzani04@gmail.com>
2023-07-22 03:45:34 +0200

    Update Homepage.jsx

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -11,29 +11,30 @@ export default function Homepage(){
         
         <div className="container-righe">
         <div className="riga">
-            <div className="foto" id='riky'></div><div className="nome">1. Riky Real</div>
+            <div className="foto" id='trenta'></div><div className="nome">1. Squenca</div>
         </div>
         <div className="riga">
-            <div className="foto" id='anza'></div><div className="nome">2. The Anza</div>
+            <div className="foto" id='riky'></div><div className="nome">2. Riky real</div>
         </div>
         <div className="riga">
-            <div className="foto" id='faso'></div><div className="nome">3. Faso</div>
+            <div className="foto" id='broggi'></div><div className="nome">3. 8roggi</div>
+        </div>
+            <div className="riga">
+            <div className="foto" id='onorevole'></div><div className="nome">4. Onorevole</div>
         </div>
         <div className="riga">
-            <div className="foto" id='garna'></div><div className="nome">4. Garna the Gabri</div>
+            <div className="foto" id='anza'></div><div className="nome">5. The anza</div>
         </div>
         <div className="riga">
-            <div className="foto" id='trenta'></div><div className="nome">5. Trenta i</div>
+            <div className="foto" id='pit'></div><div className="nome">6. Pit</div>
         </div>
         <div className="riga">
-            <div className="foto" id='broggi'></div><div className="nome">6. 8roggi</div>
+            <div className="foto" id='garna'></div><div className="nome">7. Garna the abri</div>
         </div>
         <div className="riga">
-            <div className="foto" id='onorevole'></div><div className="nome">7. Onorevole</div>
+            <div className="foto" id='faso'></div><div className="nome">8. Faso</div>
         </div>
-        <div className="riga">
-            <div className="foto" id='pit'></div><div className="nome">8. Pit</div>
-        </div>
+        
         </div>
 
         <div className="logo"></div>
